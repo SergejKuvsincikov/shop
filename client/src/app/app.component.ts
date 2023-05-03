1f7b51ec79ea8321b53b3733d0685e33ab0fe3fb
SergejKuvsincikov <sergkuvj@gmail.com>
2023-05-03 14:34:01 +0100

    added API call

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
   title = 'E-Shop';
+  constructor(private http:HttpClient) {
+     
+  }
+  products: any [];
+
+  ngOnInit(): void {
+    this.http.get('https://localhost:5001/api/products?sort=name&search=a').subscribe(     
+    (response: any ) => {
+       this.products = response.data;;
+    },
+    error => 
+    {
+      console.log(error);
+    });
+  }
 }
