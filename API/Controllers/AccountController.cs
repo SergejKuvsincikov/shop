35af922dd925d39ed82e3b05ece37e4ee0cf444f
SergejKuvsincikov <sergkuvj@gmail.com>
2023-06-28 14:27:24 +0200

    Generation of token

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -42,6 +42,29 @@ namespace API.Controllers
                 DisplayName = user.DisplayName  
             };
         }
+        [HttpPost("register")]
+        public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
+        {
+            var user = new AppUser 
+            {
+                DisplayName = registerDto.DisplayName,
+                UserName = registerDto.Email,
+                Email = registerDto.Email
+            };
+
+            var result = await _userManager.CreateAsync(user,registerDto.Password);
+
+            if(!result.Succeeded)
+            {
+                return BadRequest(new ApiResponse(400));
+            }
+
+            return new UserDto {
+                Email = user.Email,
+                Token = "Here will be the token",
+                DisplayName = user.DisplayName  
+            };
+        }
 
     }
 }
